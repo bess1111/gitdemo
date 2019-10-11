78489bc907a2f698251820e3e41b6ef0e137b3f9
weimengli <weimenglyly@163.com>
2019-10-11 16:18:34 +0800

    sssss

--- src/main/java/com/example/manager/rest/controller/classes/AaController.java
+++ src/main/java/com/example/manager/rest/controller/classes/AaController.java
@@ -1,4 +1,14 @@
 package com.example.manager.rest.controller.classes;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class AaController {
+
+
+
+
+    public static  void as(){
+        System.out.println("aaaa");
+    }
 }
